9081b26737e03750c9f7608dbaa5f7ffd16bce71
AdrianFilinski <67971511+AdrianFilinski@users.noreply.github.com>
2020-07-10 06:27:23 +0200

    added innerText

--- js/script.js
+++ js/script.js
@@ -6,4 +6,11 @@ onChangeButton.addEventListener("click", () =>{
     page.classList.toggle("bodyLight")
 })
 
+    const mailButton = document.querySelector (".js-mail")
+
+    mailButton.addEventListener("click", () => {
+        mailButton.innerText = "adrian.filinski1993@gmail.com" 
+
+    })
+
 }
